c54ebe58db94a45d8776727f6aef92bbec8cbf49
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-10-13 14:47:16 +0900

    fix: 초기세팅

---
+++ src/test/java/src/main/java/spring/hugme/HugDogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package src.main.java.spring.hugme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HugDogProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
