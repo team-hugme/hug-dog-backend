945e94dddf674c4e352bf26dfe551a63a32c53a4
Pracrobo <cpu180621@gmail.com>
2025-10-16 00:38:17 +0900

    feat: config 세팅 redis, jwt, password, querydsl, security

--- hugme-dog/src/main/java/spring/hugme/infra/config/QueryDslConfig.java
+++ hugme-dog/src/main/java/spring/hugme/infra/config/QueryDslConfig.java
@@ -10,8 +10,6 @@ public class QueryDslConfig {
 
     @Bean
     public JPAQueryFactory jpaQueryFactory(EntityManager em) {
-        // javax → jakarta 불일치 문제 때문에 생성자 직접 호출 대신
-        // 실제 QueryDSL 사용 시 JPQLQueryFactory를 직접 주입해서 사용
         return new JPAQueryFactory(() -> em.unwrap(javax.persistence.EntityManager.class));
     }
 }
