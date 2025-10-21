b2037645280ffa5fe39542b3e55ebff8da6c515e
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-10-21 18:01:16 +0900

    feat : 구조 이상한거 변경

--- hugme-dog/src/main/java/spring/hugme/domain/user/entity/Member.java
+++ hugme-dog/src/main/java/spring/hugme/domain/user/entity/Member.java
@@ -36,41 +36,22 @@ public class Member {
   private UUID id;
 
 
-  @Column(name = "user_id", nullable = false, unique = true)
   private String userId;
 
-  @Column(name = "email", nullable = false, unique = true)
   private String email;
 
-  @Column(name = "password", nullable = false)
   private String password;
 
-  @Column(name = "profile_url", length = 500)
   private String profileUrl;
 
-  @Column(name = "name", nullable = false)
   private String name;
 
-  @Column(name = "birthday")
   private LocalDate birthday;
 
-  @Column(name = "phone", length = 100)
   private String phone;
 
-  @Column(name = "active")
-  @Builder.Default
-  private Boolean active = true;
+  private String reasonWithdRaw;
 
-  @Column(name = "reason_withdraw", length = 300)
-  private String reasonWithdraw;
-
-  @CreatedDate
-  @Column(name = "create_at", updatable = false)
-  private LocalDateTime createAt;
-
-  @LastModifiedDate
-  @Column(name = "update_at")
-  private LocalDateTime updateAt;
 
   // ========================
   // 비밀번호 체크
