3a9826023122f2e09797eb12e39e64401925c999
Pracrobo <cpu180621@gmail.com>
2025-10-22 17:11:26 +0900

    fix: feedback 관련 1차 수정, api prefix 적용 관련

--- hugme-dog/src/main/java/spring/hugme/domain/auth/controller/view/AuthViewController.java
+++ hugme-dog/src/main/java/spring/hugme/domain/auth/controller/view/AuthViewController.java
@@ -5,25 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/v1/auth")
+@RequestMapping("/auth")
 public class AuthViewController {
 
     // 로그인 페이지
     @GetMapping("/login")
     public String loginPage() {
-        return "auth/login";  // templates/auth/login.html
+        return "auth/login";
     }
 
     // 회원가입 페이지
     @GetMapping("/signup")
     public String signupPage() {
-        // Model에 객체 바인딩 필요 없음 (JavaScript로 처리)
-        return "auth/signup";  // templates/auth/signup.html
+        return "auth/signup";
     }
 
-    // 메인 페이지
-    @GetMapping("/main")
-    public String mainPage() {
-        return "main";  // templates/main.html
-    }
 }
