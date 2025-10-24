4a5a7097cc878af1b7125f9886f1d469ca29393b
Pracrobo <cpu180621@gmail.com>
2025-10-24 21:58:10 +0900

    fix: member 로 엔티티 이름 변경

---
+++ hugme-dog/src/main/java/spring/hugme/domain/user/entity/Member.java
@@ -0,0 +1,62 @@
+package spring.hugme.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import spring.hugme.infra.entity.BaseEntity;
+
+@Entity
+@Table(name = "member")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @JdbcTypeCode(SqlTypes.BINARY)
+    @Column(columnDefinition = "BINARY(16)", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String profileUrl;
+
+    @Column(nullable = false)
+    private String name;
+
+    private LocalDate birthday;
+
+    private String phone;
+
+    private String reasonWithdraw;
+
+    // ========================
+    // 비밀번호 체크
+    // ========================
+    public boolean checkPassword(
+        org.springframework.security.crypto.password.PasswordEncoder passwordEncoder,
+        String rawPassword) {
+        return passwordEncoder.matches(rawPassword, this.password);
+    }
+}
