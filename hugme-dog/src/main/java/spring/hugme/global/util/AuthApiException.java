c97547f2ef7da9645ced80ccd4ebde14280305c5
Pracrobo <cpu180621@gmail.com>
2025-11-02 20:42:26 +0900

    chore: 주석 삭제

--- hugme-dog/src/main/java/spring/hugme/global/util/AuthApiException.java
+++ hugme-dog/src/main/java/spring/hugme/global/util/AuthApiException.java
@@ -1,6 +1,8 @@
 package spring.hugme.global.error.exceptions;
+
+
 import lombok.extern.slf4j.Slf4j;
-import spring.hugme.global.response.ResponseCode;
+import spring.hugme.infra.response.ResponseCode;
 
 @Slf4j
 public class AuthApiException extends CommonException {
@@ -8,6 +10,7 @@ public class AuthApiException extends CommonException {
     public AuthApiException(ResponseCode code) {
         super(code);
     }
+
     public AuthApiException(ResponseCode code, Exception e) {
         super(code, e);
         log.error(e.getMessage(), e);
