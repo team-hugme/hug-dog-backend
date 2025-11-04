9335feaa229efee8db73b94831e9f28b2a952713
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-11-04 17:23:24 +0900

    :style: style: 글 상세보기 response dto 구조 작성

--- hugme-dog/src/main/java/spring/hugme/domain/community/dto/PostDetailResponse.java
+++ hugme-dog/src/main/java/spring/hugme/domain/community/dto/PostDetailResponse.java
@@ -1,11 +1,38 @@
 package spring.hugme.domain.community.dto;
 
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 import lombok.Builder;
 import lombok.Data;
+import spring.hugme.domain.community.code.BoardAlias;
 
 @Data
 @Builder
 public class PostDetailResponse {
+  BoardAlias type;
 
+  UUID userId;
+
+  Long postId;
+
+  Long boarId;
+
+  String nickname;
+
+  List<TagInfo> tag;
+
+
+  String title;
+
+  String content;
+
+  int likeCount;
+
+  int commentCount;
+
+  LocalDateTime createdAt;
+
+  LocalDateTime updatedAt;
 }
