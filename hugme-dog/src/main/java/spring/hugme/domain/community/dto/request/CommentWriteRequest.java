4e26d8323cc977301c474a3ad76470f9c9c9a5bd
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-11-18 22:37:54 +0900

    :heavy_minus_sign: chore: 필요없는 의존성 삭제

--- hugme-dog/src/main/java/spring/hugme/domain/community/dto/request/CommentWriteRequest.java
+++ hugme-dog/src/main/java/spring/hugme/domain/community/dto/request/CommentWriteRequest.java
@@ -1,11 +1,8 @@
 package spring.hugme.domain.community.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import spring.hugme.domain.community.code.BoardAlias;
+
 
 @Data
 @NoArgsConstructor
