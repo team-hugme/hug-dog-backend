23548fcdfd242c1082d0cecf5e4c1bc7df65c2b9
Pracrobo <cpu180621@gmail.com>
2025-11-26 21:33:57 +0900

    :recycle: refactor: 로그인 로직 변경

--- hugme-dog/src/main/java/spring/hugme/domain/auth/controller/AuthController.java
+++ hugme-dog/src/main/java/spring/hugme/domain/auth/controller/AuthController.java
@@ -66,9 +66,7 @@ public class AuthController extends BaseController {
     }
 
     /**
-     * Access Token 재발급
-     * - Refresh Token을 쿠키에서 가져옴
-     * - userId는 Refresh Token에서 추출
+     * Access Token 재발급 - Refresh Token을 쿠키에서 가져옴 - userId는 Refresh Token에서 추출
      */
     @PostMapping("/reissue")
     public CommonApiResponse<LoginResponse> reissue(
@@ -84,9 +82,7 @@ public class AuthController extends BaseController {
     }
 
     /**
-     * 로그아웃
-     * - Redis에서 Refresh Token 삭제
-     * - Refresh Token 쿠키 삭제
+     * 로그아웃 - Redis에서 Refresh Token 삭제 - Refresh Token 쿠키 삭제
      */
     @PostMapping("/logout")
     public CommonApiResponse<Void> logout(
--- hugme-dog/src/main/java/spring/hugme/domain/user/entity/Member.java
+++ hugme-dog/src/main/java/spring/hugme/domain/user/entity/Member.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import java.time.LocalDate;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
