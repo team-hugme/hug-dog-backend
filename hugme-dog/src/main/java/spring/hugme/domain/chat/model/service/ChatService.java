934cf5f654e737a461bd7e9903409891000b06cf
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-12-07 18:29:44 +0900

    :sparkles: feat: 챗봇 채팅방 생성, 챗봇 응답,대화 내용 기억, 대화 기록 목록, 강아지 챗봇 목록, 챗봇대화방 삭제 하기 Service 생성

---
+++ hugme-dog/src/main/java/spring/hugme/domain/chat/model/service/ChatService.java
@@ -0,0 +1,204 @@
+package spring.hugme.domain.chat.model.service;
+
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import jakarta.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import spring.hugme.domain.chat.dto.ChatBotDogListResponse;
+import spring.hugme.domain.chat.dto.ChatBotResponse;
+import spring.hugme.domain.chat.dto.ChatMessageListResponse;
+import spring.hugme.domain.chat.dto.ChatMessageResponse;
+import spring.hugme.domain.chat.dto.ChatResponse;
+import spring.hugme.domain.chat.dto.ChatStartRequest;
+import spring.hugme.domain.chat.dto.LastMessageDto;
+import spring.hugme.domain.chat.entity.ChatBot;
+import spring.hugme.domain.chat.entity.ChatMessage;
+import spring.hugme.domain.chat.model.repo.ChatBotRepository;
+import spring.hugme.domain.chat.model.repo.ChatMessageRepository;
+import spring.hugme.domain.dog.model.entity.Dog;
+import spring.hugme.domain.dog.model.repo.DogRepository;
+import spring.hugme.domain.user.entity.Member;
+import spring.hugme.domain.user.repository.UserRepository;
+import spring.hugme.global.code.SenderType;
+import spring.hugme.global.error.exceptions.NotFoundException;
+
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+
+  private final DogAssistant dotBot;
+  private final UserRepository userRepository;
+  private final DogRepository dogRepository;
+  private final ChatBotRepository chatBotRepository;
+  private final ChatMessageRepository chatMessageRepository;
+  private final EmbeddingStore<TextSegment> embeddingStore;
+  private final EmbeddingModel embeddingModel;
+  private final RedisMessageService redisMessageService;
+
+
+  @Transactional
+  public ChatResponse LLMChatResponse(ChatStartRequest request, String userId) {
+
+    Member member = userRepository.findByUserId(userId)
+        .orElseThrow(() -> new NotFoundException("해당 멤버가 존재하지 않습니다."));
+
+    ChatBot chatBot = chatBotRepository.findById(request.getChatBotId())
+        .orElseThrow(() -> new NotFoundException("해당 챗봇이 존재하지 않습니다."));
+
+    String RainbowTrue;
+
+    if(request.isRainbowTrue()){
+      RainbowTrue = "무지개 다리를 건너 천국에 있는";
+    }else{
+      RainbowTrue = "현재 살아서 주인 옆에 있는";
+    }
+
+    String assistantMessage = dotBot.chat(request.getChatBotId(), String.valueOf(request.getDogId()),request.getDogName(), member.getName(), request.getDogFeature(), request.getDogAge(), request.getDogBreed(), RainbowTrue, request.getUserMessage());
+
+    ChatMessage humanMessage = ChatMessage.builder()
+        .member(member)
+        .chatBot(chatBot)
+        .content(request.getUserMessage())
+        .isSender(SenderType.HUMAN)
+        .build();
+
+    ChatMessage machineMessage = ChatMessage.builder()
+        .member(member)
+        .chatBot(chatBot)
+        .content(assistantMessage)
+        .isSender(SenderType.MACHINE)
+        .build();
+
+    chatMessageRepository.save(humanMessage);
+    chatMessageRepository.save(machineMessage);
+
+    redisMessageService.saveLastMessage(chatBot.getChatbotId(), machineMessage.getContent(), machineMessage.getCreatedAt());
+
+    ChatMessageResponse humunChat = ChatMessageResponse.builder()
+            .chatMessageId(humanMessage.getChatMessageId())
+            .role(SenderType.HUMAN)
+            .content(humanMessage.getContent())
+            .createdAt(humanMessage.getCreatedAt())
+            .build();
+
+    ChatMessageResponse machineChat = ChatMessageResponse.builder()
+        .chatMessageId(machineMessage.getChatMessageId())
+        .role(SenderType.MACHINE)
+        .content(machineMessage.getContent())
+        .createdAt(machineMessage.getCreatedAt())
+        .build();
+
+    return ChatResponse.builder()
+        .chatBotId(request.getChatBotId())
+        .userMessage(humunChat)
+        .assistantMessage(machineChat)
+        .build();
+
+
+
+
+  }
+
+  @Transactional
+  public ChatBotResponse ChatRoomCreate(Long dogId, String userId) {
+
+    Member member = userRepository.findByUserId(userId)
+        .orElseThrow(() -> new NotFoundException("해당 멤버가 존재하지 않습니다."));
+
+    Dog dog = dogRepository.findById(dogId)
+        .orElseThrow(() -> new NotFoundException("해당 강아지가 존재하지 않습니다"));
+
+    ChatBot chatBot = ChatBot.builder()
+        .lastMessage(null)
+        .member(member)
+        .dog(dog)
+        .build();
+
+    chatBotRepository.save(chatBot);
+
+    return ChatBotResponse.builder()
+        .userId(member.getId())
+        .dogId(dog.getDogId())
+        .chatBotId(chatBot.getChatbotId())
+        .build();
+  }
+
+  public List<ChatMessageListResponse> messageList(Long chatBotId, String userId) {
+
+    Member member = userRepository.findByUserId(userId)
+        .orElseThrow(() -> new NotFoundException("해당 멤버가 존재하지 않습니다."));
+
+    ChatBot chatBot = chatBotRepository.findById(chatBotId)
+        .orElseThrow(() -> new NotFoundException("해당 채팅방이 존재하지 않습니다"));
+
+    List<ChatMessage> messages = chatMessageRepository.findByMemberAndChatBotOrderByCreatedAtAsc(member, chatBot);
+
+    return messages.stream()
+        .map(message ->
+            ChatMessageListResponse.builder()
+                .chatMessageId(message.getChatMessageId())
+                .content(message.getContent())
+                .isSender(message.getIsSender())
+                .createdAt(message.getCreatedAt())
+                .build())
+        .toList();
+
+  }
+
+  public List<ChatBotDogListResponse> chatBotDogList(String userId) {
+
+    Member member = userRepository.findByUserId(userId)
+        .orElseThrow(() -> new NotFoundException("해당 멤버가 존재하지 않습니다."));
+
+
+    List<ChatBot> chatBots = chatBotRepository.findByMemberAndDog(member);
+
+
+    return chatBots.stream()
+        .map( chatBot -> {
+
+
+              LastMessageDto dto =redisMessageService.getLastMessage(chatBot.getChatbotId());
+              String lastMessage;
+              String createdAt;
+              if(dto == null){
+
+                lastMessage = null;
+                createdAt =null;
+
+              }else{
+                lastMessage = dto.getContent();
+                createdAt = dto.getCreatedAt();
+              }
+               return ChatBotDogListResponse.builder()
+                  .dogId(chatBot.getDog().getDogId())
+                  .chatBotId(chatBot.getChatbotId())
+                  .lastMessage(lastMessage)
+                  .userId(chatBot.getMember().getId())
+                  .dogName(chatBot.getDog().getDogName())
+                  .dogImageURl(chatBot.getDog().getImageURL())
+                  .createdAt(createdAt)
+                  .build();
+
+            }
+
+        ).toList();
+
+
+  }
+
+  @Transactional
+  public void chatBotDogDelete(Long chatBotId) {
+
+    ChatBot chatBot = chatBotRepository.findById(chatBotId)
+        .orElseThrow(() -> new NotFoundException("해당 챗봇이 존재하지 않습니다"));
+
+    chatBot.setActivated(false);
+
+  }
+}
