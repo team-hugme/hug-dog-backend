8e15eff2868cf5af2f6ca97d57d0b60918c96ae8
Pracrobo <cpu180621@gmail.com>
2025-12-07 11:53:00 +0900

    chore: 파일 이름 수정

--- hugme-dog/src/main/java/spring/hugme/infra/config/QuerydslConfig.java
+++ hugme-dog/src/main/java/spring/hugme/infra/config/QuerydslConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class QueryDslConfig {
+public class QuerydslConfig {
 
     @Bean
     public JPAQueryFactory jpaQueryFactory(EntityManager em) {
