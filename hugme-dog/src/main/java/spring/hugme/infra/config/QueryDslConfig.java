e12b4e1cb45b02a6d0d1420bbe1a1fcf9ad41194
Pracrobo <cpu180621@gmail.com>
2025-12-07 11:56:38 +0900

    chore: 파일 이름 수정

--- hugme-dog/src/main/java/spring/hugme/infra/config/QueryDslConfig.java
+++ hugme-dog/src/main/java/spring/hugme/infra/config/QueryDslConfig.java
@@ -6,13 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class QueryDslConfig {
+public class QuerydslConfig {
 
     @Bean
     public JPAQueryFactory jpaQueryFactory(EntityManager em) {
-        // javax → jakarta 불일치 문제 때문에 생성자 직접 호출 대신
-        // 실제 QueryDSL 사용 시 JPQLQueryFactory를 직접 주입해서 사용
-        return new JPAQueryFactory(() -> em.unwrap(javax.persistence.EntityManager.class));
-    }
+        return new JPAQueryFactory(em);
 }
 
