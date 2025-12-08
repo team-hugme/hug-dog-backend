96ae8f6cfe2f27ea16c0f056f8fe7ae89eacd007
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2025-12-08 19:05:22 +0900

    :recycle: refactor: 백엔드에서 가져올수 있는거 받아오는거 변경

--- hugme-dog/src/main/java/spring/hugme/domain/chat/dto/ChatStartRequest.java
+++ hugme-dog/src/main/java/spring/hugme/domain/chat/dto/ChatStartRequest.java
@@ -13,17 +13,8 @@ public class ChatStartRequest {
   // 무지개 다리 건넜는지?
   boolean rainbowTrue;
 
-  //강아지 특징
-  String dogFeature;
-
-  //강아지 이름
-  String dogName;
-
-  //강아지 나이
-  int dogAge;
-
-  //강아지 품종
-  String dogBreed;
+  //보호자 성별
+  String gender;
 
   //보낸 메세지
   String userMessage;
