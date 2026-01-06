f0ebbd24d601c15ee107fb36611c719c9074a9cd
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2026-01-06 22:13:32 +0900

    :heavy_plus_sign: chore: repository 의존성 추가

--- hugme-dog/src/main/java/spring/hugme/domain/user/repository/UserRepository.java
+++ hugme-dog/src/main/java/spring/hugme/domain/user/repository/UserRepository.java
@@ -2,10 +2,12 @@ package spring.hugme.domain.user.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import spring.hugme.domain.user.entity.Member;
+import spring.hugme.domain.user.repository.custom.UserRepositoryCustom;
 
-
-public interface UserRepository extends JpaRepository<Member, Long> {
+@Repository
+public interface UserRepository extends JpaRepository<Member, Long> , UserRepositoryCustom {
 
     Optional<Member> findByUserId(String userId);
     boolean existsByUserId(String userId);
