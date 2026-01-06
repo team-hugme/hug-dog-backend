b9fbae7b9db395f389c14abefd64671c8bb373a7
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2026-01-06 22:11:18 +0900

    :sparkles: feat:  querydsl 작성

--- hugme-dog/src/main/java/spring/hugme/domain/community/model/repo/custom/CommentRepositoryCustom.java
+++ hugme-dog/src/main/java/spring/hugme/domain/community/model/repo/custom/CommentRepositoryCustom.java
@@ -3,9 +3,10 @@ package spring.hugme.domain.community.model.repo.custom;
 import spring.hugme.domain.community.entity.Comments;
 import spring.hugme.domain.community.entity.Post;
 import java.util.List;
+import spring.hugme.domain.user.entity.Member;
 
 public interface CommentRepositoryCustom {
 
   List<Comments> findAllCommentsWithMemberByPost(Post post);
-
+  List<Comments> findByMember(Member member);
 }
