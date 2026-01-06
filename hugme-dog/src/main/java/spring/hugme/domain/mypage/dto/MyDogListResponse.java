942cebba40ad7f39c5820f7643fa119b83f040b4
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2026-01-06 22:08:47 +0900

    :sparkles: feat:  내 강아지 목록 response  생성

---
+++ hugme-dog/src/main/java/spring/hugme/domain/mypage/dto/MyDogListResponse.java
@@ -0,0 +1,35 @@
+package spring.hugme.domain.mypage.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import spring.hugme.global.code.DogGender;
+import spring.hugme.global.code.DogSize;
+
+@Data
+@Builder
+public class MyDogListResponse {
+
+  private String userId;
+
+  private Long dogId;
+
+  private String name;
+
+  private String birthday;
+
+  private String breed;
+
+  private DogGender gender;
+
+  private Boolean isNeutered;
+
+  private DogSize dogSize;
+
+  private float weight;
+
+  private Boolean isRainbow;
+
+  private Boolean chatStatus;
+
+
+}
